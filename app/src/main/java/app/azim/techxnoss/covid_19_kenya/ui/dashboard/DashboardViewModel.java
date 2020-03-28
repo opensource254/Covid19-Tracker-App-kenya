41047c36373b8007e797c30c053a59665b882b09
azim254 <verdixconie@gmail.com>
2020-03-28 22:07:24 +0300

    initial commit

---
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package app.azim.techxnoss.covid_19_kenya.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
