8827ecb8d8eb0fce997ac2f56d666ed4ccadfd32
azim254 <verdixconie@gmail.com>
2020-03-28 23:08:03 +0300

    removed fragments

--- app/src/main/java/app/azim/techxnoss/covid_19_kenya/MainActivity.java
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/MainActivity.java
@@ -20,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_stats, R.id.navigation_news)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+      // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+      //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       // NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
