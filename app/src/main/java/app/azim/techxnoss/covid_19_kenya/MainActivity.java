39df5fc72544fc93991235ed3ad2b89856557dde
azim254 <verdixconie@gmail.com>
2020-03-29 15:37:54 +0300

    added precaution page

--- app/src/main/java/app/azim/techxnoss/covid_19_kenya/MainActivity.java
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/MainActivity.java
@@ -1,6 +1,9 @@
 package app.azim.techxnoss.covid_19_kenya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,9 +25,19 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_stats, R.id.navigation_news)
                 .build();
-      // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-      //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-       // NavigationUI.setupWithNavController(navView, navController);
+
+        Button btn_precautions = (Button)findViewById(R.id.Btn_Precautions);
+
+        //starting precaution activity
+        btn_precautions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PrecautionsActivity.class));
+                //setting up slide up animation
+                overridePendingTransition(R.anim.slide_up,  R.anim.no_animation);
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/PrecautionsActivity.java
@@ -0,0 +1,67 @@
+package app.azim.techxnoss.covid_19_kenya;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.Button;
+
+public class PrecautionsActivity extends AppCompatActivity {
+
+    ConstraintLayout expandableView , expandableView1 ;
+    Button arrowBtn , arrowBtn1 ;
+    CardView cardView, cardView1 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_precautions);
+
+        expandableView = findViewById(R.id.expandableView);
+        expandableView1 = findViewById(R.id.expandableView1);
+        arrowBtn = findViewById(R.id.arrowBtn);
+        arrowBtn1 = findViewById(R.id.arrowBtn1);
+        cardView = findViewById(R.id.cardView);
+        cardView1 = findViewById(R.id.cardView1);
+
+
+        arrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableView.getVisibility()==View.GONE){
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.VISIBLE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.GONE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
+        arrowBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableView1.getVisibility()==View.GONE){
+                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
+                    expandableView1.setVisibility(View.VISIBLE);
+                    arrowBtn1.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
+                    expandableView1.setVisibility(View.GONE);
+                    arrowBtn1.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
