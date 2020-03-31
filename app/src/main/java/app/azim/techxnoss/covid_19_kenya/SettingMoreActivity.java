fed19a108bc10efb4cd33f84057aa32f59764174
azim254 <verdixconie@gmail.com>
2020-04-01 01:54:41 +0300

    added settings ui

---
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/SettingMoreActivity.java
@@ -0,0 +1,13 @@
+package app.azim.techxnoss.covid_19_kenya;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingMoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_more);
+    }
+}
