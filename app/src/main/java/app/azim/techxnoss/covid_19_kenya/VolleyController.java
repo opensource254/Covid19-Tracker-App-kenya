2d433a911ef3d1979e9a431ba9a352994304fad5
azim254 <verdixconie@gmail.com>
2020-05-26 19:09:30 +0300

    removed redundant code

--- app/src/main/java/app/azim/techxnoss/covid_19_kenya/VolleyController.java
+++ app/src/main/java/app/azim/techxnoss/covid_19_kenya/VolleyController.java
@@ -31,20 +31,9 @@ public class VolleyController extends Application {
         return mRequestQueue;
     }
 
-    public <T> void addToRequestQueue(Request<T> req, String tag) {
-        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-        getRequestQueue().add(req);
-    }
-
     public <T> void addToRequestQueue(Request<T> req) {
         req.setTag(TAG);
         getRequestQueue().add(req);
-    }
-
-    public void cancelPendingRequests(Object tag) {
-        if (mRequestQueue != null) {
-            mRequestQueue.cancelAll(tag);
-        }
     }
 
 
